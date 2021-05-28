aa5f4fbd58bb1d664af09cf42eb9c6322b8f9f0f
Ryan Pumford <rpumford@gmail.com>
2021-05-28 11:13:37 -0400

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using RazorForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RazorForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(MeaningResponse mr)
+        {
+            return View("Thanks", mr);
+        }
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/MeaningResponse.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorForm.Models
+{
+    public class MeaningResponse
+    {
+        [Required(ErrorMessage = "Please enter your name.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "You must have some thought about the meaning of life...")]
+        public string Response { get; set; }
+
+    }
+}
